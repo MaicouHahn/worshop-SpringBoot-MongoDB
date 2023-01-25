3a96693932265d7cdca8767b73680d5ced957d42
Maicou Hahn <maicouhahn11@gmail.com>
2023-01-25 10:46:28 -0300

    project created

---
+++ src/main/java/com/nelioalvez/worshopmongo/WorskhopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalvez.worshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorskhopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorskhopmongoApplication.class, args);
+	}
+
+}
