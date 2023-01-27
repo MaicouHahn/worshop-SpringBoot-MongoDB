61a3e2e790473a5bf698304909f95f1d4dcf801b
Maicou Hahn <maicouhahn11@gmail.com>
2023-01-27 19:16:11 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/nelioalvez/worshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalvez/worshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.nelioalvez.worshopmongo.domain.Post;
 import com.nelioalvez.worshopmongo.domain.User;
+import com.nelioalvez.worshopmongo.dto.AuthorDTO;
 import com.nelioalvez.worshopmongo.repository.PostRepository;
 import com.nelioalvez.worshopmongo.repository.UserRepository;
 
@@ -34,10 +35,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viajem","Vou viajar para São Paulo. Abraços",maria);
-		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!",maria);
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));//é salvo os usuarios antes para que tenham um ID proprio e depois é feito a copia do Dto abaixo
 		
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viajem","Vou viajar para São Paulo. Abraços",new AuthorDTO(maria));
+		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!",new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 
