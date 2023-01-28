1214495fee8f45462c3884999c2c573d62f85804
Maicou Hahn <maicouhahn11@gmail.com>
2023-01-27 21:41:00 -0300

    Consulta simples com @Query

--- src/main/java/com/nelioalvez/worshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalvez/worshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.nelioalvez.worshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalvez.worshopmongo.domain.Post;
@@ -10,7 +11,9 @@ import com.nelioalvez.worshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
-			
+	@Query("{'title': { $regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
